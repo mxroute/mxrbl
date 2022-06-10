1bf5057f5ea4892f802f73f231a9864fc406008c
mxroute <37432698+mxroute@users.noreply.github.com>
2022-06-09 21:47:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,5 @@ MXRBL is a DNSRBL containing IPs and IP ranges known directly to be spammers tha
 To use this blacklist, direct your DNS queries to bl.mxrbl.com. If you are not familiar with the accepted method, reverse the IP address and add ".bl.mxrbl.com" at the end. Example: 170.130.140.177 becomes 177.140.130.170.bl.mxrbl.com.
 <br><br>
 <a href="lookup.php">Click here</a> to check if an IP is listed on our blacklist.
+<br><br><br>
+June 9th Update: There may be intermittent failures where IPs that should be blacklisted appear to not be. This is due to an upgrade taking place. This message will be removed when it is complete.
